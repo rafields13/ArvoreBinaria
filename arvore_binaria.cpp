35c443a44ebd5eeddabd7980460f0a95bcfc972e
Rafael Marliere de Oliveira <126572694+rafields13@users.noreply.github.com>
2023-11-15 18:08:47 -0300

    Add files via upload
    modificacoes no codigo pra rodar liso

--- arvore_binaria.cpp
+++ arvore_binaria.cpp
@@ -4,21 +4,17 @@
 #include <algorithm>
 
 struct noArvore {
-    struct noArvore* ponteiroEsquerda;
+    struct noArvore *ponteiroEsquerda;
     int dado;
-    struct noArvore* ponteiroDireita;
+    struct noArvore *ponteiroDireita;
 };
 
-void inserirNo(struct noArvore** ponteiroPonteiroArvore, int valor) {
+void inserirNo(struct noArvore **ponteiroPonteiroArvore, int valor) {
     if (*ponteiroPonteiroArvore == nullptr) {
         *ponteiroPonteiroArvore = new struct noArvore;
-
-        if (*ponteiroPonteiroArvore != nullptr) {
-            (*ponteiroPonteiroArvore)->dado = valor;
-            (*ponteiroPonteiroArvore)->ponteiroEsquerda = nullptr;
-            (*ponteiroPonteiroArvore)->ponteiroDireita = nullptr;
-        } else
-            std::cout << "\n" << valor << " nao inserido. Nao ha memoria disponivel.\n";
+        (*ponteiroPonteiroArvore)->dado = valor;
+        (*ponteiroPonteiroArvore)->ponteiroEsquerda = nullptr;
+        (*ponteiroPonteiroArvore)->ponteiroDireita = nullptr;
     } else {
         if (valor < (*ponteiroPonteiroArvore)->dado)
             inserirNo(&((*ponteiroPonteiroArvore)->ponteiroEsquerda), valor);
@@ -27,7 +23,50 @@ void inserirNo(struct noArvore** ponteiroPonteiroArvore, int valor) {
     }
 }
 
-void preOrdem(struct noArvore* ponteiroArvore, bool raiz = true) {
+struct noArvore *buscarNo(struct noArvore *ponteiroArvore, int valor) {
+    if (ponteiroArvore == nullptr || ponteiroArvore->dado == valor)
+        return ponteiroArvore;
+
+    if (valor < ponteiroArvore->dado)
+        return buscarNo(ponteiroArvore->ponteiroEsquerda, valor);
+    else
+        return buscarNo(ponteiroArvore->ponteiroDireita, valor);
+}
+
+struct noArvore *encontrarMenorValor(struct noArvore *ponteiroArvore) {
+    while (ponteiroArvore->ponteiroEsquerda != nullptr)
+        ponteiroArvore = ponteiroArvore->ponteiroEsquerda;
+
+    return ponteiroArvore;
+}
+
+void removerNo(struct noArvore **ponteiroPonteiroArvore, int valor) {
+    if (valor < (*ponteiroPonteiroArvore)->dado)
+        removerNo(&((*ponteiroPonteiroArvore)->ponteiroEsquerda), valor);
+    else if (valor > (*ponteiroPonteiroArvore)->dado)
+        removerNo(&((*ponteiroPonteiroArvore)->ponteiroDireita), valor);
+    else {
+        if ((*ponteiroPonteiroArvore)->ponteiroEsquerda == nullptr &&
+            (*ponteiroPonteiroArvore)->ponteiroDireita == nullptr) {
+            delete *ponteiroPonteiroArvore;
+            *ponteiroPonteiroArvore = nullptr;
+        } else if ((*ponteiroPonteiroArvore)->ponteiroEsquerda == nullptr) {
+            struct noArvore *ponteiroTemporarioArvore = (*ponteiroPonteiroArvore)->ponteiroDireita;
+            delete *ponteiroPonteiroArvore;
+            *ponteiroPonteiroArvore = ponteiroTemporarioArvore;
+        } else if ((*ponteiroPonteiroArvore)->ponteiroDireita == nullptr) {
+            struct noArvore *ponteiroTemporarioArvore = (*ponteiroPonteiroArvore)->ponteiroEsquerda;
+            delete *ponteiroPonteiroArvore;
+            *ponteiroPonteiroArvore = ponteiroTemporarioArvore;
+        } else {
+            struct noArvore *ponteiroTemporarioArvore = encontrarMenorValor((*ponteiroPonteiroArvore)->ponteiroDireita);
+            (*ponteiroPonteiroArvore)->dado = ponteiroTemporarioArvore->dado;
+            removerNo(&(*ponteiroPonteiroArvore)->ponteiroDireita, ponteiroTemporarioArvore->dado);
+        }
+    }
+}
+
+void preOrdem(struct noArvore *ponteiroArvore, bool raiz = true) {
     if (ponteiroArvore != nullptr) {
         if (raiz)
             std::cout << "[" << ponteiroArvore->dado << "] ";
@@ -39,10 +78,10 @@ void preOrdem(struct noArvore* ponteiroArvore, bool raiz = true) {
     }
 }
 
-void ordem(struct noArvore* ponteiroArvore, bool raiz = true) {
+void ordem(struct noArvore *ponteiroArvore, bool raiz = true) {
     if (ponteiroArvore != nullptr) {
         ordem(ponteiroArvore->ponteiroEsquerda, false);
-        
+
         if (raiz)
             std::cout << "[" << ponteiroArvore->dado << "] ";
         else
@@ -52,7 +91,7 @@ void ordem(struct noArvore* ponteiroArvore, bool raiz = true) {
     }
 }
 
-void posOrdem(struct noArvore* ponteiroArvore, bool raiz = true) {
+void posOrdem(struct noArvore *ponteiroArvore, bool raiz = true) {
     if (ponteiroArvore != nullptr) {
         posOrdem(ponteiroArvore->ponteiroEsquerda, false);
         posOrdem(ponteiroArvore->ponteiroDireita, false);
@@ -65,7 +104,7 @@ void posOrdem(struct noArvore* ponteiroArvore, bool raiz = true) {
 }
 
 int main() {
-    struct noArvore* ponteiroRaiz = nullptr;
+    struct noArvore *ponteiroRaiz = nullptr;
 
     std::srand(std::time(nullptr));
     std::vector<int> numeros;
@@ -78,7 +117,6 @@ int main() {
     }
 
     std::cout << "\nOs numeros que serao inseridos na arvore sao: (a raiz esta destacada entre colchetes)\n";
-
     inserirNo(&ponteiroRaiz, numeros[0]);
     std::cout << "[" << numeros[0] << "] ";
 
@@ -86,6 +124,18 @@ int main() {
         inserirNo(&ponteiroRaiz, numeros[i]);
         std::cout << numeros[i] << " ";
     }
+
+    std::cout << "\n\nDigite o valor a ser removido:";
+    int valor;
+    std::cin >> valor;
+
+    struct noArvore *noEncontrado = buscarNo(ponteiroRaiz, valor);
+
+    if (noEncontrado != nullptr) {
+        removerNo(&ponteiroRaiz, valor);
+        std::cout << "Elemento removido com sucesso. Mostrando as travessias...";
+    } else
+        std::cout << "Elemento nao encontrado na arvore. Mostrando as travessias com todos os elementos...";
 
     std::cout << "\n\nTravessia pre-ordem:\n";
     preOrdem(ponteiroRaiz);
